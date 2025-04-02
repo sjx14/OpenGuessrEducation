fa4a6fa6e1bcaf4926aa06b0b143a29f39cddb1e
sjx14 <judeallenby608@gmail.com>
2025-04-02 11:38:58 +0100

    Update +page.svelte

--- src/routes/countries/learn/angola/+page.svelte
+++ src/routes/countries/learn/angola/+page.svelte
@@ -1,1 +1,5 @@
+<script>
+</script>
+
+<p class="mb-2">Angola, officially the Republic of Angola, is a country on the west-central coast of Southern Africa. It is the second-largest Portuguese-speaking (Lusophone) country in both total area and population and is the seventh-largest country in Africa.</p>
 <!-- Write or paste an article with HTML & Svelte here -->
